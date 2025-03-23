7d631b98220cf439157563af3468c7e4ddac615b
Dhanushmathan <lovelymathan2394@gmail.com>
2025-03-23 20:51:42 +0530

    new priject init

---
+++ src/main.js
@@ -0,0 +1,6 @@
+const colorToggleEL = document.getElementById("colorToggle");
+
+colorToggleEL.addEventListener('click', () => {
+  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = randomColor;
+});
